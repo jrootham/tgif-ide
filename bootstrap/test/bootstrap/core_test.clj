6a07f1058c8e5bb1d9973c697656b794454d3cd9
jrootham <jrootham@gmail.com>
2018-06-16 08:16:46 -0400

    Initial commit of boilerplate

---
+++ bootstrap/test/bootstrap/core_test.clj
@@ -0,0 +1,7 @@
+(ns bootstrap.core-test
+  (:require [clojure.test :refer :all]
+            [bootstrap.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
---
+++ ide/test/ide/core_test.clj
@@ -0,0 +1,7 @@
+(ns ide.core-test
+  (:require [clojure.test :refer :all]
+            [ide.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
